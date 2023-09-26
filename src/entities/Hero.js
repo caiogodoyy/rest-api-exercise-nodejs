2ede12b2df284576e8c55d3c704158ab70744aa7
caiogodoyy <caio.godoy.vg@gmail.com>
2023-09-26 18:32:14 -0300

    creating the hero repository and service

--- src/entities/Hero.js
+++ src/entities/Hero.js
@@ -2,11 +2,11 @@ import { randomUUID } from "node:crypto";
 
 export default class Hero {
 
-    constructor( {name, age, power} ) {
+    constructor({ name, age, power }) {
         this.id = randomUUID()
         this.name = name
         this.age = age
         this.power = power
     }
-    
+
 }
--- src/repositories/HeroRepository.js
+++ src/repositories/HeroRepository.js
@@ -1,5 +1,29 @@
-export default HeroRepository {
+import { readFile, writeFile } from "node:fs/promises";
 
-    
+export default class HeroRepository {
+
+    constructor({ file }) {
+        this.file = file
+    }
+
+    async #getDatabase() {
+        return JSON.parse(await readFile(this.file))
+    }
+
+    find() {
+        return this.#getDatabase()
+    }
+
+    async create(data) {
+        const db = this.#getDatabase
+        db.push(data)
+
+        await writeFile(
+            this.file,
+            JSON.stringify(db)
+        )
+
+        return data.id
+    }
 
 }
